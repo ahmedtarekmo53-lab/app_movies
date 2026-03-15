bd98710c742d0a2cf8c3c0e9d9b61ddc6fc61d96
AHMEDTAREK\VT <ahmedtarekmo53>
2026-03-15 23:53:16 +0200

    hive

---
+++ lib/core/models/movie_model.g.dart
@@ -0,0 +1,68 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MovieModelAdapter extends TypeAdapter<MovieModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  MovieModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MovieModel(
+      id: fields[0] as int,
+      title: fields[1] as String,
+      poster: fields[2] as String,
+      bigPoster: fields[3] as String,
+      rating: fields[4] as double,
+      year: fields[5] as int,
+      description: fields[6] as String,
+      genres: (fields[7] as List).cast<String>(),
+      runtime: fields[8] as int,
+      language: fields[9] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MovieModel obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.poster)
+      ..writeByte(3)
+      ..write(obj.bigPoster)
+      ..writeByte(4)
+      ..write(obj.rating)
+      ..writeByte(5)
+      ..write(obj.year)
+      ..writeByte(6)
+      ..write(obj.description)
+      ..writeByte(7)
+      ..write(obj.genres)
+      ..writeByte(8)
+      ..write(obj.runtime)
+      ..writeByte(9)
+      ..write(obj.language);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MovieModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
